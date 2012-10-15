5417d17a2864e90c74b4ad751754a6561d9d06a1
khabbab <khabbabs@gmail.com>
2012-10-14 23:54:16 -0600

    this is the 1st commit, adding only testing dom files. also includes the product.js. 	khabbabs@unm.edu/@gmail.com

---
+++ product.js
@@ -0,0 +1,10 @@
+
+var name,image,quantity,type,code;
+var myElement = document.getElementsByName("li");
+function product(name,image,quantity,type,code){
+    this.name=name;
+    this.image=image;
+    this.quantity=quantity;
+    this.type=type;
+    this.code=code;
+}
