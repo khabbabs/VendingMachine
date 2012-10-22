d8142d709a37a9f202c5d330d6e5beab9b60d430
Khabbab <Khabbabs@gmail.com>
2012-10-22 01:03:56 -0600

    4th commit, key pad and the change buttons on the vending machine now work

--- keyPadActionListener.js
+++ keyPadActionListener.js
@@ -1,4 +1,39 @@
-document.getElementById("keypadimg").onclick = function(){
-    alert("working");
+//var padElements= document.getElementById("keypadTable").attributes.length;
+//console.log("elements"+padElements[0]);
+var dollar,_25cents,_10cents,change;
+var totalAmount=0;
+dollar   = document.getElementById("1_dollar");
+_25cents = document.getElementById("25_cent");
+_10cents = document.getElementById("10_cent");
+change   = document.getElementById("change");
+
+
+dollar.onclick = function (){
+    updateTotalAmmount(1);
 };
 
+
+_25cents.onclick = function (){
+    updateTotalAmmount(.25);
+};
+
+
+_10cents.onclick = function (){
+    updateTotalAmmount(.10);
+};
+
+change.onclick = function (){
+    updateTotalAmmount("clear");
+};
+
+
+function updateTotalAmmount(value){
+    if(value!="clear"){
+    totalAmount+=value;
+    document.getElementById("curDisp").innerHTML=Math.round(totalAmount*100)/100;
+    }else if(value=="clear"){
+        alert("your total change is : $"+Math.round(totalAmount*100)/100);
+        document.getElementById("curDisp").innerHTML=0.00;
+        totalAmount=0;
+    }
+}
---
+++ makeDyamicKeyPad.js
@@ -0,0 +1,20 @@
+var rows = 4;
+
+var table = document.getElementById("keypadTable");
+var temptr,temptd;
+for(var i =0;i<rows;i++){
+    temptr = document.createElement("tr");
+    temptd = document.createElement("td");
+    if(i==0){
+        temptd.id="1_dollar";
+    }else if(i==2){
+        temptd.id="25_cent";
+    }else if(i==3){
+        temptd.id="10_cent";
+    }
+    else {
+        temptd.id="change";
+    }
+    table.appendChild(temptr);
+    temptr.appendChild(temptd);
+}
