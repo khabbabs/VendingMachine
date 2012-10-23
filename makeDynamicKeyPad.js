752da334ce6d4b177b5a2e4df77cabe13760cc18
khabbab <khabbabs@gmail.com>
2012-10-22 20:30:45 -0600

    new keypad

--- makeDynamicKeyPad.js
+++ makeDynamicKeyPad.js
@@ -4,17 +4,52 @@ var table = document.getElementById("keypadTable");
 var temptr,temptd;
 for(var i =0;i<rows;i++){
     temptr = document.createElement("tr");
-    temptd = document.createElement("td");
-    if(i==0){
-        temptd.id="1_dollar";
-    }else if(i==1){
-        temptd.id="25_cent";
-    }else if(i==2){
-        temptd.id="10_cent";
-    }
-    else {
-        temptd.id="change";
-    }
     table.appendChild(temptr);
+    
+    
+    
+    for(var j = 0;j<2;j++){
+       
+    temptd = document.createElement("td");
     temptr.appendChild(temptd);
+    if(j==0){
+        if(i==0){
+          temptd.id="1_dollar";
+        }else if(i==1)
+        {
+        temptd.id="50_cent";
+        }else if(i==2)
+        {
+        temptd.id="25_cent";
+        }
+        else {
+       temptd.id="10_cent";
+        } 
+     }else if(j==1){
+        if(i==0){
+          temptd.id="1_euro";
+        }else if(i==1)
+        {
+        temptd.id="50_pence";
+        }else if(i==2)
+        {
+        temptd.id="20_pence";
+        }
+        else {
+       temptd.id="10_pence";
+        }
+     }
+    }
+    //if(i==0){
+    //    temptd.id="1_dollar";
+    //}else if(i==1){
+    //    temptd.id="25_cent";
+    //}else if(i==2){
+    //    temptd.id="10_cent";
+    //}
+    //else {
+    //    temptd.id="change";
+    //}
+    
+    
 }
